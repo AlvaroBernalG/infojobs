1a7f5cfac957e9c8b0693fdd9e6227f916e6026b
Alvaro <alvaro.bernal.gimeno@gmail.com>
2017-06-29 21:10:16 +0100

    Initial

---
+++ test/test.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const infojobs = require('../index.js')
+
+let credentials = JSON.parse(fs.readFileSync('./test/credentials.json', 'utf-8'))
+
+describe('infojobs()', () => {
+
+  it('should be a high order function', () => {
+    expect(typeof infojobs(credentials) === 'function').toBe(true)
+  })
+})
+
+describe('run() | start() | go()', ()=>{
+
+  it('should return a promise', ()=>{
+    expect(infojobs(credentials)().offer().run() instanceof Promise).toBe(true)
+  })
+
+  test('whether the promise resolve value returns 20 records.', async ()=>{
+
+   let res = await infojobs(credentials)().offer().run()
+
+    expect(res.offers.length === 20).toBe(true)
+  })
+
+})
+
