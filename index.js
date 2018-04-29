eee097b05ce533cae971df8ea512890924abc8d3
Alvaro Bernal <alvaro.bernal.gimeno@gmail.com>
2018-04-29 13:13:21 +0100

    Add prettier-eslint and format script

--- index.js
+++ index.js
@@ -1,116 +1,117 @@
-const request = require('request')
-const { URL, URLSearchParams } = require('url')
+const request = require("request");
+const { URL, URLSearchParams } = require("url");
 
 const defaults = {
-  apiUrl: 'https://api.infojobs.net/api/1',
+  apiUrl: "https://api.infojobs.net/api/1",
   resources: [
-    'application',
-    'dictionary',
-    'candidate',
-    'skillcategory',
-    'offer',
-    'id',
-    'killerquestion',
-    'question',
-    'openquestion',
-    'type'
+    "application",
+    "dictionary",
+    "candidate",
+    "skillcategory",
+    "offer",
+    "id",
+    "killerquestion",
+    "question",
+    "openquestion",
+    "type"
   ]
-}
+};
 
 const chain = instance => func => (...args) => {
-  func.call(func, ...args)
-  return instance
-}
+  func.call(func, ...args);
+  return instance;
+};
 
-const requester = (auth) => (url) => new Promise((resolve, reject) => {
-  request(url, (err, response, body) => {
-    if (err) return reject(err)
-    try {
-      const res = JSON.parse(body)
-      if (res.error) return reject(res.error_description)
-      resolve(res)
-    } catch (err) {
-      reject(err)
-    }
-  }).auth(auth.id, auth.secret)
-})
+const requester = auth => url =>
+  new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (err) return reject(err);
+      try {
+        const res = JSON.parse(body);
+        if (res.error) return reject(res.error_description);
+        resolve(res);
+      } catch (err) {
+        reject(err);
+      }
+    }).auth(auth.id, auth.secret);
+  });
 
-const buildUrl = (base) => {
-  const url = new URL(base)
-  const inner = {}
-  const innerChained = chain(inner)
+const buildUrl = base => {
+  const url = new URL(base);
+  const inner = {};
+  const innerChained = chain(inner);
 
   inner.addPath = innerChained(path => {
-    url.pathname = `${url.pathname}/${path}`
-  })
+    url.pathname = `${url.pathname}/${path}`;
+  });
 
   inner.addQuery = innerChained(query => {
-    url.query = query
-    const queryEntries = Object.entries(query)
-    const params = new URLSearchParams(queryEntries)
-    url.search = params.toString()
-  })
+    url.query = query;
+    const queryEntries = Object.entries(query);
+    const params = new URLSearchParams(queryEntries);
+    url.search = params.toString();
+  });
 
-  inner.getQuery = () => url.query
+  inner.getQuery = () => url.query;
 
-  inner.toString = () => url.toString()
+  inner.toString = () => url.toString();
 
-  return inner
-}
+  return inner;
+};
 
 const infojobs = (
-    auth,
-    resources = defaults.resources,
-    apiUrl = defaults.apiUrl
-  ) => () => {
-    const get = requester(auth)
-    const url = buildUrl(apiUrl)
-    const inner = {}
-    const innerChained = chain(inner)
+  auth,
+  resources = defaults.resources,
+  apiUrl = defaults.apiUrl
+) => () => {
+  const get = requester(auth);
+  const url = buildUrl(apiUrl);
+  const inner = {};
+  const innerChained = chain(inner);
 
-    resources.forEach(resource => {
-      inner[resource] = innerChained(query => {
-        url.addPath(resource)
-        if (typeof query === 'string') return url.addPath(query)
-        if (query) url.addQuery(query)
-      })
-    })
+  resources.forEach(resource => {
+    inner[resource] = innerChained(query => {
+      url.addPath(resource);
+      if (typeof query === "string") return url.addPath(query);
+      if (query) url.addQuery(query);
+    });
+  });
 
-    inner.id = innerChained(url.addPath)
+  inner.id = innerChained(url.addPath);
 
-    inner.pages = async function*(from, until) {
-      if (from <= 0) throw new Error('`From` must be greater 0.')
-      if (until < from) throw new Error('Until can\'t be greater than from')
+  inner.pages = async function*(from, until) {
+    if (from <= 0) throw new Error("`From` must be greater 0.");
+    if (until < from) throw new Error("Until can't be greater than from");
 
-      let query = url.getQuery()
+    let query = url.getQuery();
 
-      if (until === undefined && from !== undefined) {
-        until = from
-        from = 1
-      }
-      url.addQuery({ ...query, page: from || 1 })
-      let res = await inner.go()
-      yield res
+    if (until === undefined && from !== undefined) {
+      until = from;
+      from = 1;
+    }
+    url.addQuery({ ...query, page: from || 1 });
+    let res = await inner.go();
+    yield res;
 
-      if (from === undefined && until === undefined) {
-        until = res.totalPages
-        from = res.currentPage
-      }
-
-      while (from < until) {
-        query = url.getQuery()
-        url.addQuery({ ...query, page: from + 1 })
-        yield await inner.go()
-        from += 1
-      }
+    if (from === undefined && until === undefined) {
+      until = res.totalPages;
+      from = res.currentPage;
     }
 
-    inner.go = inner.start = inner.run = () => {
-      const fullUrl = url.toString()
-      return get(fullUrl)
+    while (from < until) {
+      query = url.getQuery();
+      url.addQuery({ ...query, page: from + 1 });
+      yield await inner.go();
+      from += 1;
     }
+  };
 
-    return inner
-  }
+  inner.go = inner.start = inner.run = () => {
+    const fullUrl = url.toString();
+    return get(fullUrl);
+  };
 
-module.exports = infojobs
+  return inner;
+};
+
+module.exports = infojobs;
--- test/test.js
+++ test/test.js
@@ -1,126 +1,135 @@
-const infojobs = require('../index.js')
-const nock = require('nock')
-const test = require('ava')
+const infojobs = require("../index.js");
+const nock = require("nock");
+const test = require("ava");
 
-const credentials = require('../config/default.credentials.json')
+const credentials = require("../config/default.credentials.json");
 
-const nocker = nock('https://api.infojobs.net')
-const search = infojobs(credentials)
+const nocker = nock("https://api.infojobs.net");
+const search = infojobs(credentials);
 
-const range = (from, to ) => new Array(to - from + 1).fill(0).map((e, i) => i + from)
+const range = (from, to) =>
+  new Array(to - from + 1).fill(0).map((e, i) => i + from);
 
-test('infojobs() should be a high order function.', t => {
-  t.true(typeof infojobs(credentials) === 'function')
-})
+test("infojobs() should be a high order function.", t => {
+  t.true(typeof infojobs(credentials) === "function");
+});
 
-test.before(()=>{
-  nocker.get('/api/1/test/awesome').reply(200, {result: "works"})
-})
+test.before(() => {
+  nocker.get("/api/1/test/awesome").reply(200, { result: "works" });
+});
 
-test('infojobs() should correctly combine multiple resources.', async t => {
-  const res = await infojobs(credentials, ['test', 'awesome'])().test().awesome().run()
+test("infojobs() should correctly combine multiple resources.", async t => {
+  const res = await infojobs(credentials, ["test", "awesome"])()
+    .test()
+    .awesome()
+    .run();
 
-  t.deepEqual(res, {result : "works"})
-})
-test('should return a promise', expect => {
-  nocker.get('/api/1/offer').reply(200, require('./data/mock.offer.json'))
+  t.deepEqual(res, { result: "works" });
+});
+test("should return a promise", expect => {
+  nocker.get("/api/1/offer").reply(200, require("./data/mock.offer.json"));
 
   expect.true(
     search()
       .offer()
       .run() instanceof Promise
-  )
-})
+  );
+});
 
 test.before(() => {
-  nocker.get('/api/1/offer').reply(200, require('./data/mock.offer.json'))
-})
+  nocker.get("/api/1/offer").reply(200, require("./data/mock.offer.json"));
+});
 
-test('offer() should return the correct json.', async t => {
+test("offer() should return the correct json.", async t => {
   const res = await search()
     .offer()
-    .run()
+    .run();
 
-  t.deepEqual(res, require('./data/mock.offer.json'))
-})
+  t.deepEqual(res, require("./data/mock.offer.json"));
+});
 
 test.before(() => {
   nocker
-    .get('/api/1/offer')
-    .query({ q: 'java' })
-    .reply(200, require('./data/mock.offer.query.json'))
-})
+    .get("/api/1/offer")
+    .query({ q: "java" })
+    .reply(200, require("./data/mock.offer.query.json"));
+});
 
-test('offer([query]) should correctly query and return a json response.', async t => {
+test("offer([query]) should correctly query and return a json response.", async t => {
   const res = await search()
-    .offer({ q: 'java' })
-    .run()
+    .offer({ q: "java" })
+    .run();
 
-  t.deepEqual(res, require('./data/mock.offer.query.json'))
-})
+  t.deepEqual(res, require("./data/mock.offer.query.json"));
+});
 
 test.before(() => {
   nocker
-    .get('/api/1/offer/3b830e44a9426fb3d1410b8618fdcb')
-    .reply(200, require('./data/mock.id.json'))
-})
+    .get("/api/1/offer/3b830e44a9426fb3d1410b8618fdcb")
+    .reply(200, require("./data/mock.id.json"));
+});
 
-test('search([id]) should correctly return a json response.', async t => {
+test("search([id]) should correctly return a json response.", async t => {
   const res = await search()
-    .offer('3b830e44a9426fb3d1410b8618fdcb')
-    .run()
+    .offer("3b830e44a9426fb3d1410b8618fdcb")
+    .run();
 
-  t.deepEqual(res, require('./data/mock.id.json'))
-})
+  t.deepEqual(res, require("./data/mock.id.json"));
+});
 
 test.before(() => {
   nocker
-    .get('/api/1/offer/3b830e44a9426fb3d1410b8618fdcb')
-    .reply(200, require('./data/mock.id.json'))
-})
+    .get("/api/1/offer/3b830e44a9426fb3d1410b8618fdcb")
+    .reply(200, require("./data/mock.id.json"));
+});
 
-test('id([id]) should correctly return a json response.', async t => {
+test("id([id]) should correctly return a json response.", async t => {
   const res = await search()
     .offer()
-      .id('3b830e44a9426fb3d1410b8618fdcb')
-    .run()
+    .id("3b830e44a9426fb3d1410b8618fdcb")
+    .run();
 
-  t.deepEqual(res, require('./data/mock.id.json'))
-})
-
-test.before(() => {
-  const fromNumMocks = 1
-  const untilNumMocks = 3
-  range(fromNumMocks, untilNumMocks).forEach( index =>{
-    nocker.get('/api/1/offer')
-      .query({ page: index })
-      .reply(200, require(`./data/mock.offer.pages.${index}.json`))
-  })
-})
-
-test('pages() should correctly loop through all the results.', async t => {
-  let counter = 1
-  for await (const page of search().offer().pages(3)) {
-    t.deepEqual(page, require(`./data/mock.offer.pages.${counter}.json`))
-    counter += 1
-  }
-})
+  t.deepEqual(res, require("./data/mock.id.json"));
+});
 
 test.before(() => {
-  const fromNumMocks = 3
-  const untilNumMocks = 6
-  range(fromNumMocks, untilNumMocks).forEach(index =>{
-    nocker.get('/api/1/offer')
+  const fromNumMocks = 1;
+  const untilNumMocks = 3;
+  range(fromNumMocks, untilNumMocks).forEach(index => {
+    nocker
+      .get("/api/1/offer")
       .query({ page: index })
-      .reply(200, require(`./data/mock.offer.pages.${index}.json`))
-  })
-})
+      .reply(200, require(`./data/mock.offer.pages.${index}.json`));
+  });
+});
 
-test('pages(3,6) should correctly loop through the specified boundaries.', async t => {
-  let counter = 3
-  for await (const page of search().offer().pages(3, 6)) {
-    t.deepEqual(page, require(`./data/mock.offer.pages.${counter}.json`))
-    counter += 1
+test("pages() should correctly loop through all the results.", async t => {
+  let counter = 1;
+  for await (const page of search()
+    .offer()
+    .pages(3)) {
+    t.deepEqual(page, require(`./data/mock.offer.pages.${counter}.json`));
+    counter += 1;
   }
-})
+});
 
+test.before(() => {
+  const fromNumMocks = 3;
+  const untilNumMocks = 6;
+  range(fromNumMocks, untilNumMocks).forEach(index => {
+    nocker
+      .get("/api/1/offer")
+      .query({ page: index })
+      .reply(200, require(`./data/mock.offer.pages.${index}.json`));
+  });
+});
+
+test("pages(3,6) should correctly loop through the specified boundaries.", async t => {
+  let counter = 3;
+  for await (const page of search()
+    .offer()
+    .pages(3, 6)) {
+    t.deepEqual(page, require(`./data/mock.offer.pages.${counter}.json`));
+    counter += 1;
+  }
+});
